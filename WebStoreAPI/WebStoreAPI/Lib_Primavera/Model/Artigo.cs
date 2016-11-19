9e888f9f82ddfa989ea39e00c185069b144780d0
Daniel Silva Reis <pigmoth@gmail.com>
2016-11-19 15:42:36 +0000

    API push
    The first push of the WebStoreAPI

---
+++ WebStoreAPI/WebStoreAPI/Lib_Primavera/Model/Artigo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebStoreAPI.Lib_Primavera.Model
+{
+    public class Artigo
+    {
+        public string CodArtigo
+        {
+            get;
+            set;
+        }
+
+        public string DescArtigo
+        {
+            get;
+            set;
+        }
+
+       /* public List<Model.Armazem> Armazens
+        {
+            get;
+            set;
+        }*/
+
+    }
+}
---
+++ WebStoreAPI/WebStoreAPI/Lib_Primavera/PriIntegration.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Interop.ErpBS900;
+using Interop.StdPlatBS900;
+using Interop.StdBE900;
+using Interop.GcpBE900;
+using ADODB;
+
+namespace WebStoreAPI.Lib_Primavera
+{
+    public class PriIntegration
+    {
+        
+
+        # region Cliente
+
+        public static List<Model.Cliente> ListaClientes()
+        {
+            
+            
+            StdBELista objList;
+
+            List<Model.Cliente> listClientes = new List<Model.Cliente>();
+
+            if (PriEngine.InitializeCompany(WebStoreAPI.Properties.Settings.Default.Company.Trim(), WebStoreAPI.Properties.Settings.Default.User.Trim(), WebStoreAPI.Properties.Settings.Default.Password.Trim()) == true)
+            {
+
+                //objList = PriEngine.Engine.Comercial.Clientes.LstClientes();
+
+                objList = PriEngine.Engine.Consulta("SELECT Cliente, Nome, Moeda, NumContrib as NumContribuinte, Fac_Mor AS campo_exemplo FROM  CLIENTES");
+
+                
+                while (!objList.NoFim())
+                {
+                    listClientes.Add(new Model.Cliente
+                    {
+                        CodCliente = objList.Valor("Cliente"),
+                        NomeCliente = objList.Valor("Nome"),
+                        Moeda = objList.Valor("Moeda"),
+                        NumContribuinte = objList.Valor("NumContribuinte"),
+                        Morada = objList.Valor("campo_exemplo")
+                    });
+                    objList.Seguinte();
+
+                }
+
+                return listClientes;
+            }
+            else
+                return null;
+        }
+
+        public static Lib_Primavera.Model.Cliente GetCliente(string codCliente)
+        {
+            GcpBECliente objCli = new GcpBECliente();
+
+
+            Model.Cliente myCli = new Model.Cliente();
+
+            if (PriEngine.InitializeCompany(WebStoreAPI.Properties.Settings.Default.Company.Trim(), WebStoreAPI.Properties.Settings.Default.User.Trim(), WebStoreAPI.Properties.Settings.Default.Password.Trim()) == true)
+            {
+
+                if (PriEngine.Engine.Comercial.Clientes.Existe(codCliente) == true)
+                {
+                    objCli = PriEngine.Engine.Comercial.Clientes.Edita(codCliente);
+                    myCli.CodCliente = objCli.get_Cliente();
+                    myCli.NomeCliente = objCli.get_Nome();
+                    myCli.Moeda = objCli.get_Moeda();
+                    myCli.NumContribuinte = objCli.get_NumContribuinte();
+                    myCli.Morada = objCli.get_Morada();
+                    return myCli;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+                return null;
+        }
+
+        public static Lib_Primavera.Model.ResponseError UpdCliente(Lib_Primavera.Model.Cliente cliente)
+        {
+            Lib_Primavera.Model.ResponseError erro = new Model.ResponseError();
+           
+
+            GcpBECliente objCli = new GcpBECliente();
+
+            try
+            {
+
+                if (PriEngine.InitializeCompany(WebStoreAPI.Properties.Settings.Default.Company.Trim(), WebStoreAPI.Properties.Settings.Default.User.Trim(), WebStoreAPI.Properties.Settings.Default.Password.Trim()) == true)
+                {
+
+                    if (PriEngine.Engine.Comercial.Clientes.Existe(cliente.CodCliente) == false)
+                    {
+                        erro.Erro = 1;
+                        erro.Descricao = "O cliente não existe";
+                        return erro;
+                    }
+                    else
+                    {
+
+                        objCli = PriEngine.Engine.Comercial.Clientes.Edita(cliente.CodCliente);
+                        objCli.set_EmModoEdicao(true);
+
+                        objCli.set_Nome(cliente.NomeCliente);
+                        objCli.set_NumContribuinte(cliente.NumContribuinte);
+                        objCli.set_Moeda(cliente.Moeda);
+                        objCli.set_Morada(cliente.Morada);
+
+                        PriEngine.Engine.Comercial.Clientes.Actualiza(objCli);
+
+                        erro.Erro = 0;
+                        erro.Descricao = "Sucesso";
+                        return erro;
+                    }
+                }
+                else
+                {
+                    erro.Erro = 1;
+                    erro.Descricao = "Erro ao abrir a empresa";
+                    return erro;
+
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                erro.Erro = 1;
+                erro.Descricao = ex.Message;
+                return erro;
+            }
+
+        }
+
+
+        public static Lib_Primavera.Model.ResponseError DelCliente(string codCliente)
+        {
+
+            Lib_Primavera.Model.ResponseError erro = new Model.ResponseError();
+            GcpBECliente objCli = new GcpBECliente();
+
+
+            try
+            {
+
+                if (PriEngine.InitializeCompany(WebStoreAPI.Properties.Settings.Default.Company.Trim(), WebStoreAPI.Properties.Settings.Default.User.Trim(), WebStoreAPI.Properties.Settings.Default.Password.Trim()) == true)
+                {
+                    if (PriEngine.Engine.Comercial.Clientes.Existe(codCliente) == false)
+                    {
+                        erro.Erro = 1;
+                        erro.Descricao = "O cliente não existe";
+                        return erro;
+                    }
+                    else
+                    {
+
+                        PriEngine.Engine.Comercial.Clientes.Remove(codCliente);
+                        erro.Erro = 0;
+                        erro.Descricao = "Sucesso";
+                        return erro;
+                    }
+                }
+
+                else
+                {
+                    erro.Erro = 1;
+                    erro.Descricao = "Erro ao abrir a empresa";
+                    return erro;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                erro.Erro = 1;
+                erro.Descricao = ex.Message;
+                return erro;
+            }
+
+        }
+
+
+
+        public static Lib_Primavera.Model.ResponseError InsereClienteObj(Model.Cliente cli)
+        {
+
+            Lib_Primavera.Model.ResponseError erro = new Model.ResponseError();
+            
+
+            GcpBECliente myCli = new GcpBECliente();
+
+            try
+            {
+                if (PriEngine.InitializeCompany(WebStoreAPI.Properties.Settings.Default.Company.Trim(), WebStoreAPI.Properties.Settings.Default.User.Trim(), WebStoreAPI.Properties.Settings.Default.Password.Trim()) == true)
+                {
+
+                    myCli.set_Cliente(cli.CodCliente);
+                    myCli.set_Nome(cli.NomeCliente);
+                    myCli.set_NumContribuinte(cli.NumContribuinte);
+                    myCli.set_Moeda(cli.Moeda);
+                    myCli.set_Morada(cli.Morada);
+
+                    PriEngine.Engine.Comercial.Clientes.Actualiza(myCli);
+
+                    erro.Erro = 0;
+                    erro.Descricao = "Sucesso";
+                    return erro;
+                }
+                else
+                {
+                    erro.Erro = 1;
+                    erro.Descricao = "Erro ao abrir empresa";
+                    return erro;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                erro.Erro = 1;
+                erro.Descricao = ex.Message;
+                return erro;
+            }
+
+
+        }
+
+       
+
+        #endregion Cliente;   // -----------------------------  END   CLIENTE    -----------------------
+
+
+        #region Artigo
+
+        public static Lib_Primavera.Model.Artigo GetArtigo(string codArtigo)
+        {
+            
+            GcpBEArtigo objArtigo = new GcpBEArtigo();
+            Model.Artigo myArt = new Model.Artigo();
+
+            if (PriEngine.InitializeCompany(WebStoreAPI.Properties.Settings.Default.Company.Trim(), WebStoreAPI.Properties.Settings.Default.User.Trim(), WebStoreAPI.Properties.Settings.Default.Password.Trim()) == true)
+            {
+
+                if (PriEngine.Engine.Comercial.Artigos.Existe(codArtigo) == false)
+                {
+                    return null;
+                }
+                else
+                {
+                    objArtigo = PriEngine.Engine.Comercial.Artigos.Edita(codArtigo);
+                    myArt.CodArtigo = objArtigo.get_Artigo();
+                    myArt.DescArtigo = objArtigo.get_Descricao();
+
+                    return myArt;
+                }
+                
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        public static List<Model.Artigo> ListaArtigos()
+        {
+                        
+            StdBELista objList;
+            StdBELista totalStockArmazens;
+
+            Model.Artigo art = new Model.Artigo();
+            List<Model.Artigo> listArts = new List<Model.Artigo>();
+
+            if (PriEngine.InitializeCompany(WebStoreAPI.Properties.Settings.Default.Company.Trim(), WebStoreAPI.Properties.Settings.Default.User.Trim(), WebStoreAPI.Properties.Settings.Default.Password.Trim()) == true)
+            {
+
+                objList = PriEngine.Engine.Comercial.Artigos.LstArtigos();
+
+                
+                
+
+                while (!objList.NoFim())
+                {
+                    art = new Model.Artigo();
+                    art.CodArtigo = objList.Valor("artigo");
+                    art.DescArtigo = objList.Valor("descricao");
+                    //totalStockArmazens = PriEngine.Engine.Consulta("SELECT Armazem,STKActual, From ArtigoArmazem where Artigo='" + art.CodArtigo + "'");
+                    /*while (!totalStockArmazens.NoFim())
+                    {
+                        armazens.idArmazem = totalStockArmazens.Valor("armazem");
+                        armazens.Stock = totalStockArmazens.Valor("stkactual");
+                    }
+                    art.Armazens = armazens;*/
+                    listArts.Add(art);
+                    objList.Seguinte();
+                }
+
+                return listArts;
+
+            }
+            else
+            {
+                return null;
+
+            }
+
+        }
+
+        #endregion Artigo
+
+    }
+}
