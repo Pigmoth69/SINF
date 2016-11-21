bd790110724a00ea8d552f633c59263183648bea
Daniel Silva Reis <pigmoth@gmail.com>
2016-11-21 00:05:19 +0000

    Caralho de commit

---
+++ WebStoreAPI/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebStoreAPI.Lib_Primavera.Model;
+
+
+namespace WebStoreAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+
+        [Route("api/products")]
+        [HttpGet]
+        public IEnumerable<Lib_Primavera.Model.Product> Get()
+        {
+            return Lib_Primavera.PriIntegration.GetProducts();
+        }
+
+
+        [Route("api/products/{id}")]
+        [HttpGet]
+        public HttpResponseMessage GetProductId(string id)
+        {
+            Lib_Primavera.Model.Product products = Lib_Primavera.PriIntegration.GetProduct(id);
+            if (products == null)
+            {
+                var message = string.Format("Product with id = {0} not found", id);
+                HttpError err = new HttpError(message);
+                return Request.CreateResponse(HttpStatusCode.NotFound, err);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, products);
+            }
+        }
+
+        [Route("api/products/families")]
+        [HttpGet]
+        public String GetFamilies()
+        {
+            String s = "CARALHO ";
+            return s;
+        }
+
+    }
+}
+
---
+++ WebStoreAPI/Lib_Primavera/Model/Warehouse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebStoreAPI.Lib_Primavera.Model
+{
+    public class Warehouse
+    {
+        public string Code
+        {
+            get;
+            set;
+        }
+        public string Description
+        {
+            get;
+            set;
+        }
+        public string Adress
+        {
+            get;
+            set;
+        }
+        public string PostalCode
+        {
+            get;
+            set;
+        }
+        public string PostalCodeLocal
+        {
+            get;
+            set;
+        }
+        public string Phone
+        {
+            get;
+            set;
+        }
+    }
+}
