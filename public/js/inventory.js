2ad2dceadb2a707fdb9884540ed10f31bfe13552
Miguel Botelho <m_botelho8@hotmail.com>
2016-11-23 15:47:38 +0000

    missing API call. it now has a wheel but it needs to be activated

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -1,5 +1,8 @@
+
+
 function changeToActiveWare(code) {
     var temp = "[id='Warehouse"+code+"']";
+    var url;
     if ($(temp).attr("class") == 'armazem active') {
         $(temp).removeClass("active");
     }
@@ -9,13 +12,32 @@ function changeToActiveWare(code) {
     }
 
     // ver se há família ativa
-    // se houver, redirecionar para warehouse/id/family/id
-    // se não houver, redirecionar para warehouse/id
+    if ($(".familia.active").attr("id") != undefined) {
+        // se houver, redirecionar para warehouse/id/family/id
+        // fazer pedido ao node
+        var temp = code + "";
+        temp.substr(9, code.length);
+        url = "http://localhost:3000/warehouse/" + temp + "/family/" + $(".familia.active").attr("id");
+        $.get(url, function(data) {
+          addProductsWebPage(data);
+        });
+    }
+    else {
+        // se não houver, redirecionar para warehouse/id
+        url = "http://localhost:3000/warehouse/" + temp;
+        $.get(url, function(data) {                
+          //console.log(data);
+          addProductsWebPage(data);
+        });
+
+    }
+
 }
 
 function changeToActiveFam(code) {
     var temp = "[id='Family"+code+"']";
-    
+    var url;
+
     if ($(temp).attr("class") == 'familia active') {
         $(temp).removeClass("active");
     }
@@ -25,6 +47,78 @@ function changeToActiveFam(code) {
     }    
 
     // ver se há armazém ativo
-    // se houver, redirecionar para warehouse/id/family/id
-    // se não houver, redirecionar para family/id
+    if ($(".armazem.active").attr("id") != undefined) {
+        // se houver, redirecionar para warehouse/id/family/id
+        var temp = $(".armazem.active").attr("id") + "";
+        temp = temp.substr(9, temp.length);
+        url = "http://localhost:3000/warehouse/" + temp + "/family/" + code;
+        $.get(url, function(data) {                
+          addProductsWebPage(data);
+        });
+        
+    }
+    else {
+        url = "http://localhost:3000/family/" + code;
+        $.get(url, function(data) {                
+          addProductsWebPage(data);
+        });
+    }
+}
+
+function addProductsWebPage(products) {
+    var j = 0;
+    $("#products").remove();
+    $("#home").append("<div id='products' class='col-xs-10'></div>");
+    var temp = "";
+    for (var i = 0; i < products.length; i++,j++) {
+        if (j == 0) { //acrescentar a row inicial
+            temp += "<div class='row'>";
+            temp += "<div class='product col-xs-2'>";
+            temp += "<div class='clickableProduct'>";
+            temp += "<a href='/product/" + products[i].Code + "'>";
+            temp += "<img src='/images/product.png' alt='product image' class='row'></a>";
+            temp += "<div class='productInfo row'>";
+            temp += "<span class='productName col-xs-8'>"+products[i].Description+"</span>";
+            var temp1 = products[i].Prices.PVP1 + "";
+            temp += "<span class='col-xs-3 col-xs-offset-1'>" + temp1 +"€</span>";
+
+            temp += "</div>";
+            temp += "</div>";
+            temp += "<button class='row'>Add To Cart</button>";
+            temp += "</div>";
+        }
+        else if (j == 3) { // fechar a row inicial
+            temp += "<div class='product col-xs-2 col-xs-offset-1'>";
+            temp += "<div class='clickableProduct'";
+            temp += "<a href='/product/" + products[i].Code + "'>";
+            temp += "<img src='/images/product.png' alt='product image' class='row'></a>";
+            temp += "<div class='productInfo row'>";
+            temp += "<span class='productName col-xs-8'>"+products[i].Description+"</span>";
+            var temp1 = products[i].Prices.PVP1 + "";
+            temp += "<span class='col-xs-3 col-xs-offset-1'>" + temp1 +"€</span>";
+
+            temp += "</div>";
+            temp += "</div>";
+            temp += "<button class='row'>Add To Cart</button>";
+            temp += "</div>";
+            temp += "</div>"; //fechar a row
+            j = -1;
+        }
+        else { // caso normal
+            temp += "<div class='product col-xs-2 col-xs-offset-1'>";
+            temp += "<div class='clickableProduct'";
+            temp += "<a href='/product/" + products[i].Code + "'>";
+            temp += "<img src='/images/product.png' alt='product image' class='row'></a>";
+            temp += "<div class='productInfo row'>";
+            temp += "<span class='productName col-xs-8'>"+products[i].Description+"</span>";
+            var temp1 = products[i].Prices.PVP1 + "";
+            temp += "<span class='col-xs-3 col-xs-offset-1'>" + temp1 +"€</span>";
+
+            temp += "</div>";
+            temp += "</div>";
+            temp += "<button class='row'>Add To Cart</button>";
+            temp += "</div>";
+        }
+    }
+    $("#products").append(temp);
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var invs = require('../public/js/inventory.js');
+
 
 router.get('/', function(req, res) {
     request.get({url:"http://localhost:49822/api/warehouse", proxy : 'http://localhost:49822'}, function (error, response, wares) {
@@ -12,11 +14,13 @@ router.get('/', function(req, res) {
                     var famsT = [];
                     for (var i = 0; i < famsReal.length; i++) {
                         famsT[i] = {};
+                        /*
                         famsReal[i] = famsReal[i].replace('á','a');
                         famsReal[i] = famsReal[i].replace('é','e');
                         famsReal[i] = famsReal[i].replace('ó','o');
                         famsReal[i] = famsReal[i].replace('í','i');
                         famsReal[i] = famsReal[i].replace('ã','a');
+                        */
                         famsT[i].Code = famsReal[i];
                     }
                     res.render('inventory', {warehouses : waresReal, families : famsT});
@@ -33,13 +37,17 @@ router.get('/', function(req, res) {
 });
 
 router.get('/warehouse/:idW/family/:idF', function(req, res) {
-    var ware = "http://localhost:49822/api/products?warehouseId=" + req.params.idW + "&familyId=" + req.params.idF; 
+    var ware = "http://localhost:49822/api/products?warehouseId=" + req.params.idW + "&familyId=" + req.params.idF;
+    console.log(ware);
     request.get({url : ware, proxy : 'http://localhost:49822'}, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var temp = JSON.parse(body);
+            //console.log(temp);
             //res.render('inventory', {products : temp});
-            // apagar a div com id products
+            
             // dar-lhe replace com jquery exatamente igual, mas com os produtos de temp
+            //invs.addProductsWebPage(temp);
+            res.json(temp);
         }
         else {
             res.render('404');
@@ -52,9 +60,12 @@ router.get('/warehouse/:idW/', function(req, res) {
     request.get({url : ware, proxy : 'http://localhost:49822'}, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var temp = JSON.parse(body);
+            //console.log(temp);
             //res.render('inventory', {products : temp});
-            // apagar a div com id products
+
             // dar-lhe replace com jquery exatamente igual, mas com os produtos de temp
+            //invs.addProductsWebPage(temp);
+            res.json(temp);
         }
         else {
             res.render('404');
@@ -67,14 +78,20 @@ router.get('/family/:idF', function(req, res) {
     request.get({url : ware, proxy : 'http://localhost:49822'}, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var temp = JSON.parse(body);
+            //console.log(temp);
             //res.render('inventory', {products : temp});
-            // apagar a div com id products
+
             // dar-lhe replace com jquery exatamente igual, mas com os produtos de temp
+            //addProductsWebPage(temp);
+            res.json(temp);
         }
         else {
             res.render('404');
         }
     });
 });
+
+
+
 
 module.exports = router;
