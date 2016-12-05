754ddd6688f68a4c587c629ec99e210c4bb5792e
Miguel Botelho <m_botelho8@hotmail.com>
2016-12-05 19:31:39 +0000

    pilas grandes

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,51 +1,89 @@
-$("#editClientNameButton").click(function() {
-    $("#editClientName").show();
+$("#editClientNameButton").click(function () {
+    $("#editClientName").toggle();
 });
 
-$("#editFiscalNameButton").click(function() {
-    $("#editFiscalName").show();
+$("#editFiscalNameButton").click(function () {
+    $("#editFiscalName").toggle();
 });
 
-$("#editEmailButton").click(function() {
-    $("#editEmail").show();
+$("#editEmailButton").click(function () {
+    $("#editEmail").toggle();
 });
 
-$("#editPasswordButton").click(function() {
-    $("#editPassword").show();
+$("#editPasswordButton").click(function () {
+    $("#editPassword").toggle();
 });
 
-$("#editCountryButton").click(function() {
-    $("#editCountry").show();
+$("#editCountryButton").click(function () {
+    $("#editCountry").toggle();
 });
 
-$("#editAddressButton").click(function() {
-    $("#editAddress").show();
+$("#editAddressButton").click(function () {
+    $("#editAddress").toggle();
 });
 
-$("#editAddress2Button").click(function() {
-    $("#editAddress2").show();
+$("#editAddress2Button").click(function () {
+    $("#editAddress2").toggle();
 });
 
-$("#editDistrictButton").click(function() {
-    $("#editDistrict").show();
+$("#editDistrictButton").click(function () {
+    $("#editDistrict").toggle();
 });
 
-$("#editLocalButton").click(function() {
-    $("#editLocal").show();
+$("#editLocalButton").click(function () {
+    $("#editLocal").toggle();
 });
 
-$("#editPostalCodeButton").click(function() {
-    $("#editPostalCode").show();
+$("#editPostalCodeButton").click(function () {
+    $("#editPostalCode").toggle();
 });
 
-$("#editPhoneButton").click(function() {
-    $("#editPhone").show();
+$("#editPhoneButton").click(function () {
+    $("#editPhone").toggle();
 });
 
-$("#editPhone2Button").click(function() {
-    $("#editPhone2").show();
+$("#editPhone2Button").click(function () {
+    $("#editPhone2").toggle();
 });
 
-$("#editTaxpayNumberButton").click(function() {
-    $("#editTaxpayNumber").show();
+$("#editTaxpayNumberButton").click(function () {
+    $("#editTaxpayNumber").toggle();
 });
+
+$("#editExpeditionWayButton").click(function () {
+    $("#editExpeditionWay").toggle();
+});
+
+$("#editPaymentWayButton").click(function () {
+    $("#editPaymentWay").toggle();
+});
+
+$("#editPaymentTypeButton").click(function () {
+    $("#editPaymentType").toggle();
+});
+
+$("#editClientTypeButton").click(function () {
+    $("#editClientType").toggle();
+});
+
+function editProfile() {
+    var temp = {};
+    temp.nameClient = $("#nameClient").val();
+    temp.fiscalName = $("#fiscalName").val();
+    temp.clientType = $("#clientType").val();
+    temp.email = $("#email").val();
+    temp.password = $("#password").val();
+    temp.password2 = $("#newPassword").val();
+    temp.country = $("#country").val();
+    temp.address = $("#address").val();
+    temp.address2 = $("#address2").val();
+    temp.district = $("#district").val();
+    temp.local = $("#local").val();
+    temp.zipCode = $("#postalCode").val();
+    temp.paymentType = $("#paymentType").val();
+    temp.paymentWay = $("#paymentWay").val();
+    temp.expeditionWay = $("#expeditionWay").val();
+    temp.phone = $("#phone").val();
+    temp.phone2 = $("#phone2").val();
+    temp.taxpay = $("#taxpayNumber").val();
+}
--- routes/profile.js
+++ routes/profile.js
@@ -15,15 +15,15 @@ router.get('/', function (req, res) {
                 request.get({ url: ware, proxy: config.PROXY }, function (error, response, body) {
                     if (!error && response.statusCode == 200) {
                         var countries = JSON.parse(body);
-                        ware = "http:localhost:" + config.PORT + "api/utils/paymenttypes";
+                        ware = "http://localhost:" + config.PORT + "/api/utils/paymenttypes";
                         request.get({ url: ware, proxy: config.PROXY }, function (error, response, body) {
                             if (!error && response.statusCode == 200) {
                                 var paymentTypes = JSON.parse(body);
-                                ware = "http:localhost:" + config.PORT + "api/utils/paymentways";
+                                ware = "http://localhost:" + config.PORT + "/api/utils/paymentways";
                                 request.get({ url: ware, proxy: config.PROXY }, function (error, response, body) {
                                     if (!error && response.statusCode == 200) {
                                         var paymentWays = JSON.parse(body);
-                                        ware = "http:localhost:" + config.PORT + "api/utils/expeditionway";
+                                        ware = "http://localhost:" + config.PORT + "/api/utils/expeditionway";
                                         request.get({ url: ware, proxy: config.PROXY }, function (error, response, body) {
                                             if (!error && response.statusCode == 200) {
                                                 var expeditionWay = JSON.parse(body);
@@ -31,12 +31,18 @@ router.get('/', function (req, res) {
                                                 request.get({ url: ware, proxy: config.PROXY }, function (error, response, body) {
                                                     if (!error && response.statusCode == 200) {
                                                         var districts = JSON.parse(body);
-                                                        res.render('profile', { profile: client, countries: countries, districts: districts, expeditionWay : expeditionWay,
-                                                                                paymentTypes : paymentTypes, paymentWays : paymentWays});
+                                                        addCodes(client, paymentTypes, paymentWays, expeditionWay, countries, districts, function (result) {
+                                                            // tratar das merdas para por la
+                                                            res.render('profile', {
+                                                                profile: client, countries: countries, districts: districts, expeditionWay: expeditionWay,
+                                                                paymentTypes: paymentTypes, paymentWays: paymentWays, pWay : result.paymentWay, pType : result.paymentType,
+                                                                eWay : result.expeditionWay, country : result.country, district : result.district
+                                                            });
+                                                        });
                                                     }
                                                 });
                                             }
-                                        });
+                                        }); // aceder a todos os formulários, ver quais é que não estão vazios, e mandar essa merda para o primavera e para a db
                                     }
                                 });
                             }
@@ -45,13 +51,64 @@ router.get('/', function (req, res) {
                 });
             }
         });
-
     }
-
 });
 
 router.get('/edit', function (req, res) {
 
 });
+
+function addCodes(result, paymentTypes, paymentWays, expeditionWays, countries, districts, next) {
+    var temp = {};
+    for(var i = 0; i < paymentTypes.length; i++) {
+        if (paymentTypes[i].PaymentTypeCode == result.PaymentType) {
+            temp.paymentType = paymentTypes[i].PaymentTypeDescription;
+            paymentTypes[i].Selected = paymentTypes[i].PaymentTypeCode;
+        }
+        else {
+            paymentTypes[i].Selected = "";
+        }
+    }
+    for(var i = 0; i < paymentWays.length; i++) {
+        if (paymentWays[i].PaymentWayCode == result.PaymentWay) {
+            temp.paymentWay = paymentWays[i].PaymentWayDescription;
+            paymentWays[i].Selected = paymentWays[i].PaymentWayCode;
+        }
+        else {
+            paymentWays[i].Selected = "";
+        }
+    }
+    for(var i = 0; i < expeditionWays.length; i++) {
+        if (expeditionWays[i].ExpeditionCode == result.ExpeditionWay) {
+            temp.expeditionWay = expeditionWays[i].ExpeditionDescription;
+            expeditionWays[i].Selected = expeditionWays[i].ExpeditionCode;
+        }
+        else {
+            expeditionWays[i].Selected = "";
+        }
+    }
+    for(var i = 0; i < countries.length; i++) {
+        if (countries[i].Initials == result.Country) {
+            temp.country = countries[i].Name;
+            countries[i].Selected = countries[i].Initials;
+        }
+        else {
+            countries[i].Selected = "";
+        }
+    }
+    for(var i = 0; i < districts.length; i++) {
+        if (districts[i].DistrictCode == result.District) {
+            temp.district = districts[i].Description;
+            districts[i].Selected = districts[i].DistrictCode;
+        }
+        else {
+            districts[i].Selected = "";
+        }
+    }
+
+    if (typeof next == 'function') {
+        next(temp);
+    }
+}
 
 module.exports = router;
