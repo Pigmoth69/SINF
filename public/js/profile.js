4631e1f0372d124eaa743d1c1baada0ba4dc3eb8
Miguel Botelho <m_botelho8@hotmail.com>
2016-12-05 18:10:05 +0000

    fds puta que pariu as gajas que querem ter aula

---
+++ public/js/profile.js
@@ -0,0 +1,51 @@
+$("#editClientNameButton").click(function() {
+    $("#editClientName").show();
+});
+
+$("#editFiscalNameButton").click(function() {
+    $("#editFiscalName").show();
+});
+
+$("#editEmailButton").click(function() {
+    $("#editEmail").show();
+});
+
+$("#editPasswordButton").click(function() {
+    $("#editPassword").show();
+});
+
+$("#editCountryButton").click(function() {
+    $("#editCountry").show();
+});
+
+$("#editAddressButton").click(function() {
+    $("#editAddress").show();
+});
+
+$("#editAddress2Button").click(function() {
+    $("#editAddress2").show();
+});
+
+$("#editDistrictButton").click(function() {
+    $("#editDistrict").show();
+});
+
+$("#editLocalButton").click(function() {
+    $("#editLocal").show();
+});
+
+$("#editPostalCodeButton").click(function() {
+    $("#editPostalCode").show();
+});
+
+$("#editPhoneButton").click(function() {
+    $("#editPhone").show();
+});
+
+$("#editPhone2Button").click(function() {
+    $("#editPhone2").show();
+});
+
+$("#editTaxpayNumberButton").click(function() {
+    $("#editTaxpayNumber").show();
+});
--- routes/profile.js
+++ routes/profile.js
@@ -1,14 +1,57 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config/config.js');
+var request = require('request');
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     if (req.session.user == undefined)
         res.redirect('/login');
-    else res.render('profile');
+    else {
+        var url = "http://localhost:" + config.PORT + "/api/clients/" + req.session.user;
+        request.get({ url: url, proxy: config.PROXY }, function (error, response, wares) {
+            if (!error && response.statusCode == 200) {
+                var client = JSON.parse(wares);
+                var ware = "http://localhost:" + config.PORT + "/api/utils/countries";
+                request.get({ url: ware, proxy: config.PROXY }, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        var countries = JSON.parse(body);
+                        ware = "http:localhost:" + config.PORT + "api/utils/paymenttypes";
+                        request.get({ url: ware, proxy: config.PROXY }, function (error, response, body) {
+                            if (!error && response.statusCode == 200) {
+                                var paymentTypes = JSON.parse(body);
+                                ware = "http:localhost:" + config.PORT + "api/utils/paymentways";
+                                request.get({ url: ware, proxy: config.PROXY }, function (error, response, body) {
+                                    if (!error && response.statusCode == 200) {
+                                        var paymentWays = JSON.parse(body);
+                                        ware = "http:localhost:" + config.PORT + "api/utils/expeditionway";
+                                        request.get({ url: ware, proxy: config.PROXY }, function (error, response, body) {
+                                            if (!error && response.statusCode == 200) {
+                                                var expeditionWay = JSON.parse(body);
+                                                ware = "http://localhost:" + config.PORT + "/api/utils/districts";
+                                                request.get({ url: ware, proxy: config.PROXY }, function (error, response, body) {
+                                                    if (!error && response.statusCode == 200) {
+                                                        var districts = JSON.parse(body);
+                                                        res.render('profile', { profile: client, countries: countries, districts: districts, expeditionWay : expeditionWay,
+                                                                                paymentTypes : paymentTypes, paymentWays : paymentWays});
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+    }
+
 });
 
-router.get('/edit', function(req, res) {
-    
+router.get('/edit', function (req, res) {
+
 });
 
 module.exports = router;
