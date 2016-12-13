8f400ce360c1091f3ca91369524f049ce16108b8
joaoDavidGB <joaobaiao12@gmail.com>
2016-12-13 22:29:53 +0000

    localhost

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-PORT = 3001;
-PROXY = "http://192.168.1.124:3001";
+PORT = 49822;
+PROXY = "http://localhost:49822";
 
 module.exports = {PORT, PROXY};
